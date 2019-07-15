ef0186a507d6bb747f830fa9ced6e339b59283cf
Darren Anderson <synchronopeia@gmail.com>
2019-07-15 14:49:03 -0700

    enable processing of property names of the form "baseName.type".

--- src/lib/find-timestamp.js
+++ src/lib/find-timestamp.js
@@ -5,9 +5,10 @@ const SEPARATOR = '~';
 module.exports = (timestampsArg, propertyNameArg, langArg) => {
   if (!Array.isArray(timestampsArg)) return '';
   if (!(propertyNameArg && langArg)) return '';
+  const [propertyBaseName] = propertyNameArg.split('.');
   const requestedTimestamp = timestampsArg.find((t) => {
     const [, propertyName, lang] = t.split(SEPARATOR);
-    return ((propertyName === propertyNameArg) && (lang === langArg));
+    return ((propertyName === propertyBaseName) && (lang === langArg));
   });
   return requestedTimestamp || '';
 };
